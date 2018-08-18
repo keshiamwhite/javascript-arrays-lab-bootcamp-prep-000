1b24994ac1b3f0df2ee4321610ddfc6ccac23435
Keshia White <keshiamwhite@gmail.com>
2018-08-18 23:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendKitten(name) {
+ kittens.push(name);
+ return kittens;
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newArr = kittens.slice()
+  newArr.push(name)
+  return newArr
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
